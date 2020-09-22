0f388fe6cf400ed055c974c3df788fa4a03182dd
Michael Sliger <michaelj.sliger@gmail.com>
2020-09-22 12:05:28 -0600

    Store First works

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import tempStore from './tempStore';
+import STORE from './StoreItem';
 
 class Store extends React.Component {
-
+    // LIST OF ITEMS
     render() {
+        const contentArr = STORE.map(el => {
+            return <StoreItem obj={el} />
+        })
         return (
+            <div className="store-container">
             <h2>Store</h2>
+                <ul className="store-results">
+                    {contentArr}
+                </ul>
+            </div>
         )
     }
 }
---
+++ src/components/Store/tempStore.js
@@ -0,0 +1,11 @@
+const STORE = [
+    {
+        name: 'An item',
+        imgs: [],
+        description: 'lorem ipsum mf',
+        
+
+    }
+]
+
+// dummy store for frontend work
